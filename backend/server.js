213268b908c0a740725e621fec6597576f1a4c29
Thet Htar Zin <thethtarzin2001@gmail.com>
2025-12-02 21:16:53 +0200

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
+  max: 300,
   message: 'Too many requests from this IP, please try again later.'
 });
 
